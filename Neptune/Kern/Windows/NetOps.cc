21b5576892f9a9783eef298159f11e80aea79805
ASMlover <asmlover@126.com>
2017-08-13 18:06:54 +0800

    :bug: fix(Unicode): fixed building error about using UNICODE for Windows

--- Neptune/Kern/Windows/NetOps.cc
+++ Neptune/Kern/Windows/NetOps.cc
@@ -107,11 +107,11 @@ namespace addr {
     char buf[64]{};
     if (addr->sa_family == AF_INET) {
       const struct sockaddr_in* addr4 = to_v4(addr);
-      InetNtop(AF_INET, (PVOID)&addr4->sin_addr, buf, sizeof(buf));
+      InetNtopA(AF_INET, (PVOID)&addr4->sin_addr, buf, sizeof(buf));
     }
     else if (addr->sa_family == AF_INET6) {
       const struct sockaddr_in6* addr6 = to_v6(addr);
-      InetNtop(AF_INET6, (PVOID)&addr6->sin6_addr, buf, sizeof(buf));
+      InetNtopA(AF_INET6, (PVOID)&addr6->sin6_addr, buf, sizeof(buf));
     }
     return buf;
   }
@@ -120,7 +120,7 @@ namespace addr {
       const char* ip, std::uint16_t port, struct sockaddr_in* addr) {
     addr->sin_family = AF_INET;
     addr->sin_port = Neptune::h2n16(port);
-    if (InetPton(AF_INET, ip, &addr->sin_addr) <= 0)
+    if (InetPtonA(AF_INET, ip, &addr->sin_addr) <= 0)
       CHAOSLOG_SYSERR << "NetOps::addr::get_address(ipv4) - failed";
   }
 
@@ -128,7 +128,7 @@ namespace addr {
       const char* ip, std::uint16_t port, struct sockaddr_in6* addr) {
     addr->sin6_family = AF_INET6;
     addr->sin6_port = Neptune::h2n16(port);
-    if (InetPton(AF_INET6, ip, &addr->sin6_addr) <= 0)
+    if (InetPtonA(AF_INET6, ip, &addr->sin6_addr) <= 0)
       CHAOSLOG_SYSERR << "NetOps::addr::get_address(ipv6) - failed";
   }
 }
